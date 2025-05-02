e4ce41b4361cefd704dfbe6d2817e58759263102
amirnairdrop2 <amirnairdrop2@gmail.com>
2025-05-02 19:45:25 +0330

    Create daily.sh
    Added a line to log.txt for daily GitHub streak.
    Trying to maintain 7-day commit streak.

---
+++ daily.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd brahmGAN || exit
+echo "Daily activity on $(date '+%Y-%m-%d')" >> log.txt
+git add log.txt
+git commit -m "Daily activity on $(date '+%Y-%m-%d')"
+git push origin main
